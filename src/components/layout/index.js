a7d66224b86cec701b5b29b15156296a6f30f68e
Brandon Montealegre <sneid123@hotmail.com>
2025-06-30 02:01:05 -0500

    se optimizo el codigo un poco

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,3 +1,4 @@
 // Exportar todos los componentes de layout
 export { default as Navbar } from './Navbar';
-export { default as ProtectedRoute } from './ProtectedRoute'; 
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as MainLayout } from './MainLayout'; 
